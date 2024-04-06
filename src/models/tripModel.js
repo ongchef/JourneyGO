5987d911f6956ea5886cbd44e02dd34a391abc95
r12725036 <r12725036@ntu.edu.tw>
2024-04-07 00:03:25 +0800

    get group by userid api

--- src/models/tripModel.js
+++ src/models/tripModel.js
@@ -1,1 +1,18 @@
 import db from './connection_db.js';
+
+export const getGroupByUserId = (userId) => {
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT tg.group_name, tg.start_date, tg.end_date, tg.status, c.country_name
+        FROM trip_groups tg
+        JOIN group_member gm ON tg.group_id = gm.g_id
+        JOIN group_country gc ON tg.group_id = gc.g_id
+        JOIN country c ON gc.c_id = c.country_id
+        WHERE gm.u_id = ?;`, [userId], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
--- src/routes/trip.js
+++ src/routes/trip.js
@@ -1,7 +1,23 @@
 import express from "express";
 import {
+    getGroupByUserId,
 } from "../controllers/trip.js"
 
 const router = express.Router();
 
+export const getGroup = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const data = await getGroupByUserId(userId);
+
+        // no spot found
+        if (data.length === 0){
+            return res.status(404).json({ message: "Cannot found data by given userId."});
+        }
+        
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({ message: error.message});
+    }
+}
 
