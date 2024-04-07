1209b0bd83473c3ccb07210abad659e074209bb8
r12725036 <r12725036@ntu.edu.tw>
2024-04-07 20:48:06 +0800

    finish homepage api

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,6 +1,10 @@
 import {
     getGroupByUserId,
     createGroup,
+    createInvitation,
+    getOverviewByGroupId,
+    getInvitationByUserId,
+    updateInvitation
 } from "../models/tripModel.js";
 
 export const getGroup = async (req, res) => {
@@ -10,7 +14,7 @@ export const getGroup = async (req, res) => {
 
         // no spot found
         if (data.length === 0){
-            return res.status(404).json({ message: "Cannot found data by given userId."});
+            return res.status(404).json({ message: "Cannot found groups by given userId."});
         }
         
         return res.status(200).json(data);
@@ -30,3 +34,65 @@ export const createGroup = async (req, res) => {
     }
 }
 
+export const createInvitation = async (req, res) => {
+    const {inviterId, inviteeId, groupId} = req.body;
+    try {
+        const newGroup = await createInvitation(inviterId, inviteeId, groupId);
+
+        return res.status(201).json(newGroup);
+    }catch (error) {
+        return res.status(500).json({ message: error.message});
+    }
+}
+
+export const getGroupOverview = async (req, res) => {
+    const { groupId } = req.params;
+    try {
+        const data = await getOverviewByGroupId(groupId);
+
+        // no spot found
+        if (data.length === 0){
+            return res.status(404).json({ message: "Cannot found overviews by given groupId."});
+        }
+        
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({ message: error.message});
+    }
+}
+
+export const getInvitation = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const data = await getInvitationByUserId(userId);
+
+        // no spot found
+        if (data.length === 0){
+            return res.status(404).json({ message: "Cannot found invitations by given userId."});
+        }
+        
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({ message: error.message});
+    }
+}
+
+export const putInvitation = async (req, res) => {
+    const { invitationId } = req.params;
+    const { status } = req.body;
+
+    try {
+        const updateInvitation = await updateInvitation(invitationId, status);
+
+        return res.status(201).json(updateInvitation);
+    }catch (error) {
+        return res.status(500).json({ message: error.message});
+    }
+}
+
+
+
+//GET get group: how to know who is the group owner
+//POST create group: when to add group country
+//POST create invitation: do we need to add group_id in the invitation
+//POST create invitation: the status char
--- src/routes/trip.js
+++ src/routes/trip.js
@@ -2,10 +2,18 @@ import express from "express";
 import {
     getGroup,
     createGroup,
-
+    createInvitation,
+    getGroupOverview,
+    getInvitation,
+    putInvitation,
 } from "../controllers/trip.js"
 
 const router = express.Router();
 
 router.get("/users/:userId/trip-groups", getGroup);
 router.post("/users/trip-groups", createGroup);
+router.post("//trip-groups/invitations", createInvitation);
+router.get("/trip-groups/:groupId/overview", getGroupOverview);
+router.get("/users/:userId/invitations", getInvitation);
+router.put("/invitations/:invitationId/status", putInvitation);
+
