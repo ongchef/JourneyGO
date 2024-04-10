3ba6bd63afd566aad4208281ea5aeb223dae06aa
gordonsung <r12725021@ntu.edu.tw>
2024-04-10 21:07:45 +0800

    PUT /api/tripgroup/details DELETE /api/tripgroup/{groupId}/member/{userId} Error Handling

--- src/controllers/tripgroup.js
+++ src/controllers/tripgroup.js
@@ -48,7 +48,9 @@ export const updateTripGroupDetail = async (req, res) => {
       end_date
     );
 
-    return res.status(200).json(resdata);
+    return res
+      .status(200)
+      .json({ ...resdata, message: "Update Successfully." });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -58,15 +60,19 @@ export const deleteTripGroupMember = async (req, res) => {
   const { groupId, userId } = req.params;
   try {
     const data = await getTripGroupMember(groupId, userId);
+    console.log(data);
     if (data.length === 0) {
       console.log("user", userId, " not in group", groupId);
       return res.status(404).json({
         message: "Delete Failed. Cannot found data by given groupId.",
       });
     }
-    data = await deleteTripGroupMemberbyIds(groupId, userId);
+    const resdata = await deleteTripGroupMemberbyIds(groupId, userId);
 
-    return res.status(200).json(data);
+    return res.status(200).json({
+      ...resdata,
+      message: "user " + userId + " leave group" + groupId + " successfully",
+    });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
