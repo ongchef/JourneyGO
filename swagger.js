ae101895340a29365272587d94594a76013bae21
gordonsung <r12725021@ntu.edu.tw>
2024-04-10 20:29:58 +0800

    Swagger UI

---
+++ swagger.js
@@ -0,0 +1,20 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "My API",
+    description: "Description",
+  },
+  host: "localhost:3000",
+};
+
+const outputFile = "./swagger-output.json";
+const routes = ["./src/app.js"];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
