f50752b71a3df9e81d12aac8d6f8d387776fbe29
gordonsung <r12725021@ntu.edu.tw>
2024-04-10 13:15:47 +0800

    add Swagger Files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import dotenv from "dotenv";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import axios from "axios";
+import swaggerUi from "swagger-ui-express";
+import { createRequire } from "module";
 
-
-import db from './models/db_connection.js';
-import {app} from './app.js';
+import db from "./models/db_connection.js";
+import { app } from "./app.js";
 
 dotenv.config();
+const require = createRequire(import.meta.url);
+const swaggerDocument = require("./swagger.json");
 
 export const server = createServer(app);
 const io = new Server(server, {
@@ -18,8 +21,9 @@ const io = new Server(server, {
 });
 
 const port = process.env.PORT || 3001;
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-/*=====================  SOCKET  =====================*/ 
+/*=====================  SOCKET  =====================*/
 
 io.on("connection", (socket) => {
   // New user has connected
@@ -28,38 +32,37 @@ io.on("connection", (socket) => {
   socket.on("enter_room", (data) => {
     socket.join(data.groupId);
     console.log(`${data.username} enters ${data.groupId} successfully.`);
-  })
+  });
 
   // Receive client spot seq change
   socket.on("client_spot_change", async (data) => {
     io.to(data.groupId).emit("server_spot_change", {
-      day: data.day, 
-      spot_sequence: data.spot_sequence
+      day: data.day,
+      spot_sequence: data.spot_sequence,
     });
-    
+
     // Call API update DB rows
-    var cnt = 0
+    var cnt = 0;
     data.spot_sequence.forEach(async (element) => {
-      const response = await axios.post(`http://localhost:${port}/api/tripgroup/${data.groupId}/days/${data.day}/spots`, {
-        spotId: element, 
-        sequence: cnt
-      });
-      cnt += 1
-    })
-  })
+      const response = await axios.post(
+        `http://localhost:${port}/api/tripgroup/${data.groupId}/days/${data.day}/spots`,
+        {
+          spotId: element,
+          sequence: cnt,
+        }
+      );
+      cnt += 1;
+    });
+  });
 });
 
-
-
-/*=====================  SERVER START  =====================*/ 
+/*=====================  SERVER START  =====================*/
 server.listen(port, async () => {
-  try{
+  try {
     await db.connect();
     console.log(`Server running on port http://localhost:${port}.`);
     console.log(`PostgreSQL connection established successfully.`);
-  }catch(error){
+  } catch (error) {
     console.log(error);
   }
-})
-
-
+});
