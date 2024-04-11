e1aafb927dcd217a732670d361a384a2346505f9
gordonsung <r12725021@ntu.edu.tw>
2024-04-12 02:04:39 +0800

    tripGroup API with auth

--- src/controllers/tripgroup.js
+++ src/controllers/tripgroup.js
@@ -7,6 +7,8 @@ import {
 
 export const getTripGroupDetail = async (req, res) => {
   const { groupId } = req.params;
+  const verify = req.userID;
+  console.log(verify);
   try {
     const data = await getTripGroupDetailbyGroupID(groupId);
     // no tripGroup found
@@ -15,18 +17,19 @@ export const getTripGroupDetail = async (req, res) => {
         .status(404)
         .json({ message: "Cannot found data by given groupId." });
     }
-    console.log(data);
+    //console.log(data);
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 export const updateTripGroupDetail = async (req, res) => {
-  const { groupId, groupName, start_date, end_date, userId } = req.body;
+  const { userClerkId } = req.userID;
+  const { groupId, groupName, start_date, end_date } = req.body;
   try {
     //update 之前先 get get 看
     const data = await getTripGroupDetailbyGroupID(groupId);
-    const user = await getTripGroupMember(groupId, userId);
+    const user = await getTripGroupMember(groupId, userClerkId);
     // no tripGroup found
     if (data.length === 0) {
       console.log("Cannot found group by given groupId.");
@@ -38,7 +41,8 @@ export const updateTripGroupDetail = async (req, res) => {
     if (user.length === 0) {
       console.log("Cannot found user by given userId.");
       return res.status(404).json({
-        message: "Update Failed. User " + userId + " not in group " + groupId,
+        message:
+          "Update Failed. User " + data[0].u_id + " not in group " + groupId,
       });
     }
     const resdata = await updateTripGroupDetailbyGroupId(
@@ -58,11 +62,10 @@ export const updateTripGroupDetail = async (req, res) => {
 
 export const deleteTripGroupMember = async (req, res) => {
   //console.log(req.params);
-  //const { userClerkId } = req.userID;
-  const { groupId, userId } = req.params;
+  const { userClerkId } = req.userID;
+  const { groupId } = req.params;
   try {
-    const data = await getTripGroupMember(groupId, userId);
-    console.log(66, data);
+    const data = await getTripGroupMember(groupId, userClerkId);
     if (data.length === 0) {
       console.log("user", " not in group", groupId);
       return res.status(404).json({
