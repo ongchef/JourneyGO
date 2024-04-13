7456b3da48976e067dd9597baf44c70e2627cc4d
gordonsung <r12725021@ntu.edu.tw>
2024-04-13 18:50:12 +0800

    Middleware update

--- src/middlewares/userInfo.js
+++ src/middlewares/userInfo.js
@@ -5,23 +5,43 @@ export default async function getUserInfo(req, res, next) {
   // ClerkExpressRequireAuth returns an error for unauthorized requests
   // Optionally ClerkExpressWithAuth returns an empty user with no error
   // ClerkExpressWithAuth(),
+  console.log("---------------");
+  // console.log("req", req.auth);
+  console.log("req_Id", req.auth.userId);
 
   const getToken = req.auth.getToken;
   //get user jwt template
   const template = "user";
 
   const token = await getToken({ template });
-  if (!token) {
+
+  console.log("token", token);
+  if (!token && !req.auth.userId) {
     return res.status(401).json({
       success: false,
       message: "Unauthorized",
     });
   }
-  const data = parseJwt(token);
-  console.log(data);
-  req.userID = data.userID;
-  req.email = data.email;
-  req.userName = data.userName;
+
+  if (token != null) {
+    const data = parseJwt(token);
+    // console.log("data123", data);
+    req.userID = data.userID;
+    req.email = data.email;
+    req.userName = data.userName;
+  } else {
+    // console.log("down");
+    req.userID = req.auth.sessionClaims.userID;
+    req.email = req.auth.sessionClaims.userEmail;
+    req.userName = req.auth.sessionClaims.userName;
+    // console.log("req.userId", req.userID);
+    // console.log("req.email", req.email);
+    // console.log("req.userName", req.userName);
+  }
+  console.log("req.userId", req.userID);
+  console.log("req.email", req.email);
+  console.log("req.userName", req.userName);
+
   next();
   //add user request
   //next();
