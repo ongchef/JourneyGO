5eae01359361e371712ee2fb2298f759dfd1d030
gordonsung <r12725021@ntu.edu.tw>
2024-04-16 11:44:27 +0800

    updateInvitationStatus Fix

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,25 +14,49 @@ export const addNewUser = ({ userID, userEmail, userName, status }) => {
 };
 
 export const getuserIdbyClerkId = (clerkId) => {
-  return db.query(`
+  return db.query(
+    `
     SELECT user_id From user_account WHERE clerk_user_id = $1;
-  `, [clerkId]);
+  `,
+    [clerkId]
+  );
 };
 
 export const getGroupByUserId = (userId) => {
-  return db.query(`
+  return db.query(
+    `
     SELECT tg.group_id, tg.group_name, tg.start_date, tg.end_date, tg.status
     FROM trip_groups tg
     JOIN group_member gm ON tg.group_id = gm.g_id
     WHERE gm.u_id = $1;
-  `, [userId]);
+  `,
+    [userId]
+  );
 };
 
-
-export const createGroupModel = (userId, groupName, startDate, endDate) => {
+export const createGroupModel = (
+  userId,
+  groupName,
+  countries,
+  startDate,
+  endDate
+) => {
   return new Promise((resolve, reject) => {
     db.tx(async (t) => {
-      
+      const groupCountries = [];
+
+      // Step 1: Get country_ids for each country_name
+      for (const countryName of countries) {
+        const countryId = await t.oneOrNone(
+          `SELECT country_id FROM country WHERE country_name = $1`,
+          [countryName]
+        );
+        if (countryId) {
+          groupCountries.push({ country_id: countryId.country_id });
+        } else {
+          reject(`Country ${countryName} not found in the database.`);
+        }
+      }
 
       // Step 2: Insert group into trip_groups table
       const { group_id: groupId } = await t.one(
@@ -41,7 +65,15 @@ export const createGroupModel = (userId, groupName, startDate, endDate) => {
          RETURNING group_id`,
         [groupName, startDate, endDate]
       );
-      console.log("1");
+
+      // Step 3: Insert into group_country table for each country_id
+      for (const groupCountry of groupCountries) {
+        await t.none(
+          `INSERT INTO group_country (g_id, c_id)
+           VALUES ($1, $2)`,
+          [groupId, groupCountry.country_id]
+        );
+      }
 
       // Step 4: Insert into group_member table
       await t.none(
@@ -49,27 +81,22 @@ export const createGroupModel = (userId, groupName, startDate, endDate) => {
          VALUES ($1, $2)`,
         [userId, groupId]
       );
-      console.log("2");
 
       return groupId;
     })
       .then((groupId) => {
-        console.log("groupId", groupId);
         resolve(groupId);
       })
       .catch((error) => {
-        console.log("error", error);
         reject(error);
       });
   });
 };
 
 export const getInviteeIdByEmail = (email) => {
-  console.log(email);
-  return db.oneOrNone(
-    `SELECT user_id FROM user_account WHERE email = $1`,
-    [email]
-  );
+  return db.oneOrNone(`SELECT user_id FROM user_account WHERE email = $1`, [
+    email,
+  ]);
 };
 
 export const getInvitationByUserId = (userId) => {
@@ -85,23 +112,24 @@ export const getInvitationByUserId = (userId) => {
 
 export const updateInvitation = (invitationId, status) => {
   return db.tx(async (t) => {
-    await t.none(
-      `UPDATE invitation SET status = $1 WHERE invitation_id = $2`,
-      [status, invitationId]
-    );
+    await t.none(`UPDATE invitation SET status = $1 WHERE invitation_id = $2`, [
+      status,
+      invitationId,
+    ]);
 
-    if (status === "Y") {
+    if (status === "accepted") {
       const invitationInfo = await t.oneOrNone(
-        `SELECT group_id, invitee_id FROM invitation WHERE invitation_id = $1`,
+        `SELECT g_id, invitee FROM invitation WHERE invitation_id = $1`,
         [invitationId]
       );
-
+      console.log(invitationInfo);
       if (invitationInfo) {
-        const { group_id: groupId, invitee_id: inviteeId } = invitationInfo;
-        await t.none(
-          `INSERT INTO group_member (u_id, g_id) VALUES ($1, $2)`,
-          [inviteeId, groupId]
-        );
+        const { g_id: groupId, invitee: invitee } = invitationInfo;
+
+        await t.none(`INSERT INTO group_member (u_id, g_id) VALUES ($1, $2)`, [
+          invitee,
+          groupId,
+        ]);
       }
     }
   });
