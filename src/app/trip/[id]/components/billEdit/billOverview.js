f9af32abd066b5d5de24835263707272e6c97f15
Mia Chu <mecalforever@gmail.com>
2024-05-06 15:37:21 +0800

    add post write off transaction API

--- src/app/trip/[id]/components/billEdit/billOverview.js
+++ src/app/trip/[id]/components/billEdit/billOverview.js
@@ -2,32 +2,31 @@ import { memo } from "react";
 import BillPanel from "./billPanel";
 import MyBill from "./mybill";
 import BillList from "./billList";
-import { DataContext } from '@/app/components/dataContext';
+import { DataContext } from "@/app/components/dataContext";
 import { getTransactionResult } from "@/services/getTransactionResult";
 
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext, useEffect } from "react";
+import { PropaneSharp } from "@mui/icons-material";
 
-const BillOverview = ({ group_id }) => {
-  const [userBalance, setUserBalance] = useState(0);
-  const [transactionResult, setTransactionResult] = useState([]);
+const BillOverview = ({ group_id, reloadTabPanel }) => {
+    const [userBalance, setUserBalance] = useState(0);
+    const [transactionResult, setTransactionResult] = useState([]);
 
-  const { Token } = useContext(DataContext);
+    const { Token } = useContext(DataContext);
 
-  useEffect(() => {
-    fetchTransactions();
-  }, []); 
-  
+    useEffect(() => {
+        fetchTransactions();
+    }, []);
 
     async function fetchTransactions() {
         try {
             const data = await getTransactionResult(Token, group_id);
-            console.log('group transaction result:', data);
+            console.log("group transaction result:", data);
             if (data && data.length !== 0) {
                 setUserBalance(data.balance);
                 setTransactionResult(data.transactions);
             } else {
                 console.error("No group transaction result data found");
-
             }
         } catch (error) {
             console.error("Error fetching group transaction result:", error);
@@ -39,7 +38,7 @@ const BillOverview = ({ group_id }) => {
             <div>
                 <div>
                     <div>
-                        <BillPanel group_id={group_id} />
+                        <BillPanel group_id={group_id} reloadTabPanel={reloadTabPanel} />
 
                         <div>
                             {/* <div style={{paddingTop:'20px', position: 'fixed', top: '50px', bottom: '50px', width: '100%', overflowY: 'auto' }}> */}
@@ -51,11 +50,11 @@ const BillOverview = ({ group_id }) => {
             <div style={{ flex: "1", overflowY: "auto", paddingTop: "20px" }}>
                 <div style={{ paddingBottom: "35px" }}>
                     <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-                        <BillList transactionResult={transactionResult}/>
+                        <BillList group_id={group_id} transactionResult={transactionResult} reloadTabPanel={reloadTabPanel} />
                     </div>
                 </div>
             </div>
         </div>
     );
-}
-export default memo(BillOverview);
+};
+export default BillOverview;
