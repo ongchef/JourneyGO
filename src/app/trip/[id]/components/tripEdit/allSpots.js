8e4c1ad770280a4add8bd8ed16c0083133ef8cc5
r12725016 <r12725016@ntu.edu.tw>
2024-05-08 14:44:19 +0800

    fix socket config

--- src/app/trip/[id]/components/tripEdit/allSpots.js
+++ src/app/trip/[id]/components/tripEdit/allSpots.js
@@ -13,7 +13,7 @@ export default function AllSpots({day}) {
   const [newCards, setNewCards] = useState([]); //store spot_sequence from socket
   const [newDay, setNewDay] = useState();       //store day from socket
 
-  const socket = io(process.env.NEXT_PUBLIC_BASE_URL);
+  const socket = io(process.env.NEXT_PUBLIC_BASE_URL,{transports:['websocket']});
 
   const spotChange = (_day, updateCards) => {
     const spot_sequence = updateCards?.map(card => card.id);
